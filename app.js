6e7aea3107da919bd425af25839ac13c1a85ed9e
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-27 13:28:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+let user = {
+  name: 'John',
+  sayHi: function () {
+    console.log(this.name);
+  }
+};
+
+(user.sayHi)(); // John เพราะในfunctionใช้ this ซึ่งจะ referถึงobjที่เรียกใช้methodนี้ ซึ่งในที่นี้คือ user เพราะฉะนั้นจึงคืนค่าออกมาเป็น user.name
